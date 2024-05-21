eba6675775c57811fbf7b8e42b7f7d72fc1609d5
Kozgor <igorechekqwerty@gmail.com>
2024-05-21 15:12:57 +0300

    feature: auth router protection, AppLayout skeleton

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,13 @@ import { UserProvider } from '@auth0/nextjs-auth0/client'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <UserProvider>
-    <Component {...pageProps} />
-  </UserProvider>
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return (
+    <UserProvider>
+      {getLayout(<Component {...pageProps} />, pageProps)}
+    </UserProvider>
+  )
 }
 
 export default MyApp
